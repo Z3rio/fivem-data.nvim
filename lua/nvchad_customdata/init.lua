1d76476fa71a3bf8eabb7c9c86adb14186769664
Zerio <neoboij@gmail.com>
2023-09-11 17:54:04 +0200

    add back removed files

--- lua/nvchad_customdata/init.lua
+++ lua/nvchad_customdata/init.lua
@@ -2,9 +2,7 @@ local M = {}
 
 M.getPaths = function()
     return {
-        vim.fn.stdpath("data") .. "/lazy/NvChad-customdata/fivem/Lua/natives/CFX-Native",
-        vim.fn.stdpath("data") .. "/lazy/NvChad-customdata/fivem/Lua/natives/GTAV",
-        vim.fn.stdpath("data") .. "/lazy/NvChad-customdata/fivem/Lua/natives/RDR3",
+        vim.fn.stdpath("data") .. "/lazy/NvChad-customdata/fivem/Lua",
         vim.fn.stdpath("data") .. "/lazy/NvChad-customdata/esx/Lua",
         vim.fn.stdpath("data") .. "/lazy/NvChad-customdata/qbcore/Lua",
     }
