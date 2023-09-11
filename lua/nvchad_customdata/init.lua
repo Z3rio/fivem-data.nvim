7f9d0e704e0e7ec0fe18a39597eebaf789e09b73
Zerio <neoboij@gmail.com>
2023-09-11 17:51:00 +0200

    Update init.lua

--- lua/nvchad_customdata/init.lua
+++ lua/nvchad_customdata/init.lua
@@ -2,7 +2,9 @@ local M = {}
 
 M.getPaths = function()
     return {
-        vim.fn.stdpath("data") .. "/lazy/NvChad-customdata/fivem/Lua",
+        vim.fn.stdpath("data") .. "/lazy/NvChad-customdata/fivem/Lua/natives/CFX-Native",
+        vim.fn.stdpath("data") .. "/lazy/NvChad-customdata/fivem/Lua/natives/GTAV",
+        vim.fn.stdpath("data") .. "/lazy/NvChad-customdata/fivem/Lua/natives/RDR3",
         vim.fn.stdpath("data") .. "/lazy/NvChad-customdata/esx/Lua",
         vim.fn.stdpath("data") .. "/lazy/NvChad-customdata/qbcore/Lua",
     }
