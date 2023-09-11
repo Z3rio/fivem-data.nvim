990f77c32e302af6ec8c3e9d3fb9193b6765eb21
Zerio <neoboij@gmail.com>
2023-09-11 17:28:22 +0200

    fix `getPaths` function

--- lua/init.lua
+++ lua/init.lua
@@ -1,7 +1,11 @@
-function getPaths()
+local M = {}
+
+M.getPaths = function()
     return {
         vim.fn.stdpath("data") .. "/lazy/NvChad-customdata/fivem/Lua",
         vim.fn.stdpath("data") .. "/lazy/NvChad-customdata/esx/Lua",
         vim.fn.stdpath("data") .. "/lazy/NvChad-customdata/qbcore/Lua",
     }
 end
+
+return M
