51785bf7edbcb8a207618dcf1e1a32f50e8ae263
Zerio <neoboij@gmail.com>
2023-09-21 17:45:25 +0200

    Create fxmanifest.lua

---
+++ fivem/Lua/cfx-runtime/fxmanifest.lua
@@ -0,0 +1,65 @@
+---@meta
+
+---@param game string
+function game(game) end
+
+---@param fx_version string
+function fx_version(fx_version) end
+
+---@param version string
+function version(version) end
+
+---@param author string
+function author(author) end
+
+---@param description string
+function description(description) end
+
+---@param toggle string
+function lua54(toggle) end
+
+---@param files string[]
+function escrow_ignore(files) end
+
+---@param files string[]
+function shared_scripts(files) end
+
+---@param files string[]
+function client_scripts(files) end
+
+---@param files string[]
+function server_scripts(files) end
+
+---@param files string[]
+function files(files) end
+
+---@param file string
+function ui_page(file) end
+
+---@param file string
+---@return function
+function data_file(file) end
+
+---@param toggle string
+function this_is_a_map(toggle) end
+
+---@param toggle string
+function server_only(toggle) end
+
+---@param file string
+function loadscreen(file) end
+
+---@param file string
+function file(file) end
+
+---@param file string | string[]
+function dependency(file) end
+
+---@param files string | string[]
+function dependencies(files) end
+
+---@param resource string
+function provides(resource) end
+
+---@param resources string | string[]
+function provides(resources) end
